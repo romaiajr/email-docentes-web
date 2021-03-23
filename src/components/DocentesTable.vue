b4dbcafa078b57d2a67d1de2d6e3b5fe50561b18
Roberto Maia <59492898+romaiajr@users.noreply.github.com>
2021-03-23 20:10:57 -0300

    Update DocentesTable.vue

--- src/components/DocentesTable.vue
+++ src/components/DocentesTable.vue
@@ -7,17 +7,17 @@
         sm="12
       "
       >
-        <template>
+        <!--<template>
           <div class="ad-container">
             <Adsense
-              data-ad-client="ca-pub-1234567891234567"
+              data-ad-client="ca-pub-4302003355134660"
               data-ad-slot="1234567890"
               data-ad-format="auto"
               :data-full-width-responsive="true"
             >
             </Adsense>
           </div>
-        </template>
+        </template>-->
       </v-col>
       <v-col offset-md="2" md="8" sm="12">
         <v-card>
